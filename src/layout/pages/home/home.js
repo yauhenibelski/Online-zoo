2a9c726c47abd9bb6c9d860068be6a6b48dcfaa2
yauhenibelski <108809026+yauhenibelski@users.noreply.github.com>
2023-02-12 21:29:30 +0100

    feat: Add section_4 of the home page

--- src/layout/pages/home/home.js
+++ src/layout/pages/home/home.js
@@ -2,6 +2,7 @@ import { createElem } from '../../../utils/helper';
 import section1 from './section_1/section_1.html';
 import section2 from './section_2/section_2.html';
 import section3 from './section_3/section_3.html';
+import section4 from './section_4/section_4.html';
 import { addHomePagePats } from './section_3/section_3';
 
 (() => {
@@ -15,5 +16,6 @@ import { addHomePagePats } from './section_3/section_3';
   homePage.insertAdjacentHTML('beforeend', section1);
   homePage.insertAdjacentHTML('beforeend', section2);
   homePage.insertAdjacentHTML('beforeend', section3);
+  homePage.insertAdjacentHTML('beforeend', section4);
   addHomePagePats();
 })();
